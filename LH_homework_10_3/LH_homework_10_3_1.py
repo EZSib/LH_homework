bf2ab5745c886a5884d4424df3da92aef3f76b44
Efim_w <efimzigunov@gmail.com>
2023-05-09 00:08:04 +0600

    upd 10_3_1 добавление записей в ручную, след.комит ндао добавить ф-цию

--- LH_homework_10_3/LH_homework_10_3_1.py
+++ LH_homework_10_3/LH_homework_10_3_1.py
@@ -3,7 +3,7 @@ import hashlib
 from tkinter import *
 from tkinter import messagebox as mb
 
-conn1 = sqlite3.connect('users_new_data.db')
+conn1 = sqlite3.connect('questions_new_data.db')
 cur1 = conn1.cursor()
 cur1.execute('''CREATE TABLE IF NOT EXISTS questions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -26,4 +26,5 @@ q1.core = ' 2 + 2 * 2= '
 q1.a = '8'
 q1.b = '6'
 q1.c = '16'
-
+cur1.execute('INSERT INTO questions (question, a, b, c) VALUES (?, ?, ?, ?)', [q1.core, q1.a, q1.b, q1.c])
+conn1.commit()
