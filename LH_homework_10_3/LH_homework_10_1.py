82af46787871dcb2d11b31562378c55366ddf91a
Efim_w <efimzigunov@gmail.com>
2023-05-09 14:03:27 +0600

    upd 10_3_1 добавление стандартных записей (удалить позже) upd 10_3 переименовал интерфейс с албанского на русский

--- LH_homework_10_3/LH_homework_10_1.py
+++ LH_homework_10_3/LH_homework_10_1.py
@@ -3,7 +3,7 @@ import hashlib
 from tkinter import *
 from tkinter import messagebox as mb
 
-conn = sqlite3.connect('../test_data.db')
+conn = sqlite3.connect('test_data.db')
 cur = conn.cursor()
 
 cur.execute('''CREATE TABLE IF NOT EXISTS list_users (
@@ -24,68 +24,69 @@ def register():
     password = pass_sha256(ent_pass.get())
     cur.execute('INSERT INTO list_users (login, password) VALUES (?, ?)', [login, password])
     conn.commit()
-    mb.showinfo('Registraziya', 'Polzavatelb ' + login + ' yspeshno zaregistrirovan!')
+    mb.showinfo('Регистрация', 'Пользователь ' + login + ' успешно зарегистрирован!')
 
 
 def auth_window():
     def user_auth():
         login = ent_login_auth.get().strip()
         password = pass_sha256(ent_pass_auth.get())
-        cur.execute('SELECT password FROM list_users WHERE login = ? AND password = ?', [login, password])
-        info = cur.fetchall()
-        if len(info) > 0:
-            mb.showinfo('Avtorizaciya', 'Dobro pozhalovatb')
+        cur.execute('SELECT id FROM list_users WHERE login = ? AND password = ?', [login, password])
+        info = cur.fetchone()
+        info = info[0]
+        if info > 0:
+            mb.showinfo('Авторизация', 'Добро пожаловать')
         else:
-            mb.showerror('Avtorizaciya', 'Nevernii parolb')
+            mb.showerror('Авторизация', 'Неверный пароль')
 
     auth = Toplevel()
-    auth.geometry('360x360')
-    auth.title('Avtorizaciya')
+    auth.geometry('400x400')
+    auth.title('Авторизация')
     auth.resizable(0, 0)
 
-    lbl_main = Label(auth, text='Avtorizaciya', font='calibri 20')
+    lbl_main = Label(auth, text='Авторизация', font='calibri 20')
     lbl_main.place(x=20, y=20)
 
-    lbl_login = Label(auth, text='Login', font='calibri 14')
+    lbl_login = Label(auth, text='Логин', font='calibri 14')
     lbl_login.place(x=20, y=100)
 
     ent_login_auth = Entry(auth, width=14, font='calibri 14')
     ent_login_auth.place(x=80, y=100)
 
-    lbl_pass = Label(auth, text='Parolb', font='14')
+    lbl_pass = Label(auth, text='Пароль', font='14')
     lbl_pass.place(x=20, y=160)
 
     ent_pass_auth = Entry(auth, width=14, font='calibri 14')
     ent_pass_auth.place(x=80, y=160)
 
-    btn_auth = Button(auth, text='Voiti', font='calibri 16', command=user_auth)
+    btn_auth = Button(auth, text='Войти', font='calibri 16', command=user_auth)
     btn_auth.place(x=20, y=200)
 
 
 root = Tk()
-root.geometry('360x360')
-root.title('Registaciya')
+root.geometry('400x400')
+root.title('Регистрация')
 root.resizable(0, 0)
 
-lbl_main = Label(text='Registaciya', font='calibri 20')
+lbl_main = Label(text='Регистрация', font='calibri 20')
 lbl_main.place(x=20, y=20)
 
-lbl_login = Label(text='Login', font='calibri 14')
+lbl_login = Label(text='Логин', font='calibri 14')
 lbl_login.place(x=20, y=100)
 
 ent_login = Entry(width=14, font='calibri 14')
 ent_login.place(x=80, y=100)
 
-lbl_pass = Label(text='Pass', font='calibri 14')
-lbl_pass.place(x=20, y=160)
+lbl_pass = Label(text='Пароль', font='calibri 14')
+lbl_pass.place(x=16, y=160)
 
 ent_pass = Entry(width=14, font='calibri 14')
 ent_pass.place(x=80, y=160)
 
-btn_register = Button(text='Zaregitsya', font='calibri 16', command=register)
+btn_register = Button(text='Зарегистрироваться', font='calibri 16', command=register)
 btn_register.place(x=20, y=200)
 
-btn_auth = Button(text='Voiti', font='calibri 16', command=auth_window)
+btn_auth = Button(text='Войти', font='calibri 16', command=auth_window)
 btn_auth.place(x=20, y=270)
 
 root.mainloop()
--- LH_homework_10_3/LH_homework_10_3_1.py
+++ LH_homework_10_3/LH_homework_10_3_1.py
@@ -8,6 +8,7 @@ cur1 = conn1.cursor()
 cur1.execute('''CREATE TABLE IF NOT EXISTS questions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     question VARCHAR NOT NULL UNIQUE,
+    theme VARCHAR NOT NULL,
     a VARCHAR NOT NULL,
     b VARCHAR NOT NULL,
     c VARCHAR NOT NULL)''')
@@ -15,16 +16,36 @@ cur1.execute('''CREATE TABLE IF NOT EXISTS questions (
 
 
 class Question():
-    def __init__(self, core: str, a: str, b: str, c: str):
+    def __init__(self, core: str,theme:str, a: str, b: str, c: str):
         self.string = core
+        self.theme = theme
         self.a = a
         self.b = b
         self.c = c
 
+#добавляем в базу стандартные вопросы
 q1 = Question
 q1.core = ' 2 + 2 * 2= '
 q1.a = '8'
 q1.b = '6'
 q1.c = '16'
-cur1.execute('INSERT INTO questions (question, a, b, c) VALUES (?, ?, ?, ?)', [q1.core, q1.a, q1.b, q1.c])
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', [q1.core,'math', q1.a, q1.b, q1.c])
 conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['3 + 3 * 3 ** 3= ','math', 'q1.a', 'q1.b', 'q1.c'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['сколько дней недели в апреле?','math', '30', '7', '31'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['какие из нижеперечисленных высказываний НЕ относятся к гачи культуре:','mem', '300 bucks', 'COME ON lets go', 'Sorry, i am chicken'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['на пенек сел...', 'mem', 'Потом встал', 'Касарь должен был отдать', 'Ну раз сел, так и сиди'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['ну че, как там с деньгами?', 'mem', 'Это служба безопасности сбербанка,ваши средства в опасности, срочно переведите их на ... безопасный счет!', 'С какими деньгами?!', 'Ты кому звонишь сопля?!'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['мы ребята удалые - лазим в щели половые - ?', 'beasts', 'Строители разлившие бетон', 'Сёстры Вачовски', 'Тараканы'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['Cобака - 3, кошка - 3, корова - 2, петух - 8, Д.А.Медведев - ?', 'beasts', 'Наша страна не раз проходила через серьёзные испытания: и печенеги её терзали, и половцы', 'Вы держитесь здесь, вам всего доброго, хорошего настроения и здоровья!', '5'])
+conn1.commit()
+cur1.execute('INSERT INTO questions (question,theme, a, b, c) VALUES (?, ?, ?, ?, ?)', ['что появилось раньше, курица или яйцо?','beasts', 'Курица', 'Олды', 'Яйцо'])
+conn1.commit()
+
+
