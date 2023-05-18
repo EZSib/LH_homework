852c33032e7c03123ce00ccb2dd24f727a73af77
Efim_w <efimzigunov@gmail.com>
2023-05-18 17:57:56 +0600

    try docs 12_1

--- LH_homework_12/LH_homework_12_3.py
+++ LH_homework_12/LH_homework_12_3.py
@@ -3,20 +3,24 @@ from itertools import cycle
 from random import randint
 import os
 
+
 def resizeImage(img, newWidth, newHeight):
     old_width = img.width()
     old_height = img.height()
     new_photo_image = PhotoImage(width=newWidth, height=newHeight)
     for x in range(newWidth):
         for y in range(newHeight):
-            xOld = int(x*old_width/newWidth)
-            yOld = int(y*old_height/newHeight)
+            xOld = int(x * old_width / newWidth)
+            yOld = int(y * old_height / newHeight)
             rgb = '#%02x%02x%02x' % img.get(xOld, yOld)
             new_photo_image.put(rgb, (x, y))
     return new_photo_image
 
+
 def end_see():
     root.destroy()
+
+
 def next_rave():
     global alpha, root
     alpha += 1
@@ -26,52 +30,6 @@ def next_rave():
     result_iter_galery = next(forward)
     for _ in range(alpha):
         result_iter_galery = next(forward)
-    def resizeImage(img, newWidth, newHeight):
-        old_width = img.width()
-        old_height = img.height()
-        new_photo_image = PhotoImage(width=newWidth, height=newHeight)
-        for x in range(newWidth):
-            for y in range(newHeight):
-                xOld = int(x*old_width/newWidth)
-                yOld = int(y*old_height/newHeight)
-                rgb = '#%02x%02x%02x' % img.get(xOld, yOld)
-                new_photo_image.put(rgb, (x, y))
-        return new_photo_image
-
-    def end_see():
-        root.destroy()
-
-    root = Tk()
-    root.geometry('900x600')
-    root.title('Галерея')
-    root.resizable(0, 0)
-    root.configure(background='black')
-    myCanvas = Canvas(root, width=555, height=550)
-    myCanvas.place(x=140, y=50)
-    puppyImage = PhotoImage(file=result_iter_galery)
-    puppyImage = resizeImage(puppyImage, 555, 550)
-    myCanvas.create_image(50, 50, anchor=NW, image=puppyImage)
-    start_btn = Button(text="Закончить просмотр",bg='black', fg='pink', font='calibri 30', command=end_see)
-    start_btn.place(x=190, y=0, height=100, width=515)
-
-    start_left = Button(text="НАЗАД", bg='black', fg='yellow', height=30, width=23, command=prev_rave)
-    start_left.pack(side=LEFT)
-
-    start_right = Button(text="ВПЕРЕД",bg='black', fg='yellow', height=30,width=24, command=next_rave)
-    start_right.pack(side=RIGHT)
-
-    root.mainloop()
-
-def prev_rave():
-    global alpha, root
-    alpha += 1
-    root.destroy()
-    galery = [i for i in os.listdir() if i.endswith('.png')]
-    forward = cycle(reversed(galery))
-    result_iter_galery = next(forward)
-    for _ in range(alpha):
-        result_iter_galery = next(forward)
-
 
     def resizeImage(img, newWidth, newHeight):
         old_width = img.width()
@@ -110,8 +68,55 @@ def prev_rave():
     root.mainloop()
 
 
-galery = [i for i in os.listdir() if  i.endswith('.png') ]
-alpha = randint(0,len(galery)-1)
+def prev_rave():
+    global alpha, root
+    alpha += 1
+    root.destroy()
+    galery = [i for i in os.listdir() if i.endswith('.png')]
+    forward = cycle(reversed(galery))
+    result_iter_galery = next(forward)
+    for _ in range(alpha):
+        result_iter_galery = next(forward)
+
+    def resizeImage(img, newWidth, newHeight):
+        old_width = img.width()
+        old_height = img.height()
+        new_photo_image = PhotoImage(width=newWidth, height=newHeight)
+        for x in range(newWidth):
+            for y in range(newHeight):
+                xOld = int(x * old_width / newWidth)
+                yOld = int(y * old_height / newHeight)
+                rgb = '#%02x%02x%02x' % img.get(xOld, yOld)
+                new_photo_image.put(rgb, (x, y))
+        return new_photo_image
+
+    def end_see():
+        root.destroy()
+
+    root = Tk()
+    root.geometry('900x600')
+    root.title('Галерея')
+    root.resizable(0, 0)
+    root.configure(background='black')
+    myCanvas = Canvas(root, width=555, height=550)
+    myCanvas.place(x=140, y=50)
+    puppyImage = PhotoImage(file=result_iter_galery)
+    puppyImage = resizeImage(puppyImage, 555, 550)
+    myCanvas.create_image(50, 50, anchor=NW, image=puppyImage)
+    start_btn = Button(text="Закончить просмотр", bg='black', fg='pink', font='calibri 30', command=end_see)
+    start_btn.place(x=190, y=0, height=100, width=515)
+
+    start_left = Button(text="НАЗАД", bg='black', fg='yellow', height=30, width=23, command=prev_rave)
+    start_left.pack(side=LEFT)
+
+    start_right = Button(text="ВПЕРЕД", bg='black', fg='yellow', height=30, width=24, command=next_rave)
+    start_right.pack(side=RIGHT)
+
+    root.mainloop()
+
+
+galery = [i for i in os.listdir() if i.endswith('.png')]
+alpha = randint(0, len(galery) - 1)
 root = Tk()
 root.geometry('900x600')
 root.title('Галерея')
@@ -122,11 +127,10 @@ myCanvas.place(x=140, y=50)
 puppyImage = PhotoImage(file=galery[alpha])
 puppyImage = resizeImage(puppyImage, 555, 550)
 myCanvas.create_image(50, 50, anchor=NW, image=puppyImage)
-start_btn = Button(text="Закончить просмотр",bg='black', fg='pink', font='calibri 30', command=end_see)
+start_btn = Button(text="Закончить просмотр", bg='black', fg='pink', font='calibri 30', command=end_see)
 start_btn.place(x=190, y=0, height=100, width=515)
-start_left = Button(text="НАЗАД", bg='black', fg='yellow', height=30,width=23,command=prev_rave)
+start_left = Button(text="НАЗАД", bg='black', fg='yellow', height=30, width=23, command=prev_rave)
 start_left.pack(side=LEFT)
-start_right = Button(text="ВПЕРЕД", bg='black', fg='yellow', height=30,width=24, command=next_rave)
+start_right = Button(text="ВПЕРЕД", bg='black', fg='yellow', height=30, width=24, command=next_rave)
 start_right.pack(side=RIGHT)
 root.mainloop()
-
